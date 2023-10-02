8b838f51f28214938ccf9ecb6255b12693bc4630
abrahanbadel <abrahan.badel@cecar.edu.co>
2023-10-02 11:43:12 -0500

    ejemplos usando js y html

--- js/utilidades.js
+++ js/utilidades.js
@@ -1,17 +1,35 @@
-function crearNodo(tipoNodo){
-
+function crearNodo(tipoNodo) {
     var nodo = document.createElement(tipoNodo);
     return nodo;
 
-// return document.createElement(tipoNodo);
+    // return document.createElement(tipoNodo);
 
 }
 
 function crearNodoTexto(tipoNodo, texto) {
-
     var nodo = document.createElement(tipoNodo);
     var textoNodo = document.createTextNode(texto);
     nodo.appendChild(textoNodo);
-    document.body.appendChild(nodo);
+    return nodo;
+}
 
+function adicionarNodoBody(nodo) {
+    document.body.appendChild(nodo);
+}
+
+function crearNodoImagen(url, alt) {
+    var nodo = crearNodo("img");
+    nodo.src = url;
+    nodo.alt = alt;
+    return nodo;
+}
+
+function adicionarNodoContenedor(contenedor, nodo) {
+    contenedor.appendChild(nodo);
+}
+
+function crearNodoA(texto, href) {
+    var nodo = crearNodoTexto("a", texto);
+    nodo.href = href
+    return nodo;
 }
